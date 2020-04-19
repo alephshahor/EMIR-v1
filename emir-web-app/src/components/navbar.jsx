089f2e317c2df853746d16f286cd9a48868b52b8
alephshahor <adrianalvarezleon@gmail.com>
2020-04-19 12:27:47 +0100

    Created react-app and canvas component

---
+++ emir-web-app/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="https://pbs.twimg.com/profile_images/483307375248424960/HaEZPYK8_400x400.jpeg" width="30" height="30" class="d-inline-block align-top" alt=""/>
+                    EMIR
+                </a>
+            </nav>
+        </div>
+    )
+}
