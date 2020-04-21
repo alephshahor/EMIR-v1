88624f38a1ed86607a0a4cb872bd0a6f2986b257
alephshahor <adrianalvarezleon@gmail.com>
2020-04-21 17:54:10 +0100

    Created script to convert .csv from hours/min/sec to deg

--- dat_to_csv.py
+++ dat_to_csv.py
@@ -1,26 +1,24 @@
 import csv 
-import sys
 import argparse
 
 
 parser = argparse.ArgumentParser(prog="dat_to_csv", description="Transform a .dat file into a .csv file")
 
-parser.add_argument('input_file_path', help="The input file location")
+parser.add_argument('input_file_path', help="The input file full path (.dat)")
 
-parser.add_argument('out_file_name', help="The output file name")
+parser.add_argument('out_file_path', help="The output file full path (.csv)")
 
 args = parser.parse_args()
 
-file_path = args.input_file_path 
-export_name = args.out_file_name
+input_path = args.input_file_path 
+export_path = args.out_file_path
 
 stripped_lines = []
 
-for line in open(file_path).readlines():
+for line in open(input_path).readlines():
     line = line.strip().split()
     stripped_lines.append(line)
 
-with open("./" + export_name + ".csv", "w") as output_file:
-    writer = csv.writer(output_file)
+with open(export_path, "w", newline='') as output_path:
+    writer = csv.writer(output_path)
     writer.writerows(stripped_lines)
- 
---
+++ hms_to_deg.py
@@ -0,0 +1,38 @@
+import csv 
+import argparse
+
+
+import numpy as np  
+from astropy import units as u
+from astropy.coordinates import Angle
+
+parser = argparse.ArgumentParser(prog="dat_to_csv", description="Transform a .dat file into a .csv file")
+
+parser.add_argument('input_file_path', help="The input .csv file full path ( hours / mins / seconds)")
+
+parser.add_argument('out_file_path', help="The output .csv file full path ( deg )")
+
+args = parser.parse_args()
+
+
+input_path = args.input_file_path 
+export_path = args.out_file_path
+
+deg_coordinates = []
+
+with open(input_path, 'r') as input_file:
+
+    csv_reader = csv.reader(input_file)
+    # We pass the header as we do not convert that.
+    next(csv_reader)
+
+    for line in csv_reader:
+        line[0] = Angle(line[0]).degree
+        line[1] = Angle(line[1]).degree
+        print(line)
+        deg_coordinates.append(line)
+
+
+with open(export_path, "w", newline='') as output_file:
+    writer = csv.writer(output_file)
+    writer.writerows(deg_coordinates)
