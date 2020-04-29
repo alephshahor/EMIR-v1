f5f5d0111ab3a779ee639e8dcbf2af73d6b8966a
alephshahor <adrianalvarezleon@gmail.com>
2020-04-30 00:16:40 +0100

    Created emir region rectangle

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import argparse
 import pandas as pd
 from bokeh.plotting import ColumnDataSource, figure, output_file, show
 from bokeh.models import ColumnDataSource, CustomJS
+from bokeh.models.tools import BoxEditTool
 from bokeh.layouts import row
 
 parser = argparse.ArgumentParser(prog="app", description="")
@@ -13,14 +14,22 @@ args = parser.parse_args()
 
 input_path = args.input_file_path 
 
+selection_region_source = ColumnDataSource({
+    'x': [0], 'y': [0], 'width': [0.5],
+    'height': [0.5], 'alpha': [0.5]
+})
 
 df = pd.read_csv(input_path)
 
-
 output_file("app.html")
 s1 = ColumnDataSource(data=df)
-f1 = figure(background_fill_color="lightgrey", tools="box_select", title="Stellar points")
+f1 = figure(background_fill_color="lightgrey", title="Stellar points")
+r1 = f1.rect('x', 'y', 'width', 'height', source=selection_region_source, alpha='alpha')
+tool = BoxEditTool(renderers=[r1])
+f1.add_tools(tool)
 f1.circle(x='right_ascension', y='declination', size=5, alpha=0.7,  source=s1)
+#f1.quad(top=[0.5], bottom=[1], left=[0.5], right=[1])
+
 
 s2 = ColumnDataSource(data=dict(x=[], y=[]))
 f2 = figure(tools="", title="Watch Here")
